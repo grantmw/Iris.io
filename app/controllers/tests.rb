94b39190384236bb28f5ba026da9ad20bc7153df
Daniel Huang & Grant Wong <pair+danielkyh.grantmw@devbootcamp.com>
2015-10-05 21:39:44 -0700

    add mvp

--- app/controllers/tests.rb
+++ app/controllers/tests.rb
@@ -11,11 +11,84 @@ get '/users/:u_id/tests' do
 end
 
 get '/users/:u_id/tests/vision' do
-  @level = 2080
-  @list = "R     D     O     E     N"
+    p session[:line]
+  if not session[:line]
+    session[:line] = 0
+    @list = "ZA     O     B     I"
+    @font_size = 200
+
+    erb :input
+  end
+
+  if session[:line] == 1
+    @list = "RDOEN"
+    @font_size = 21.33956714
+    erb :input
+  elsif session[:line] == 0
+    @list = "ZAOBI"
+    @font_size = 16.00467536
+    erb :input
+  elsif session[:line] == 2
+    @list = "ANIKL"
+    @font_size = 10.66978357
+    erb :input
+  elsif session[:line] == 3
+    @list = "YGOPC"
+    @font_size = 8.535826857
+    erb :input
+  elsif session[:line] == 4
+    @list = "XZVHE"
+    @font_size = 6.401870143
+    erb :input
+  elsif session[:line] == 5
+    @list = "CTBUD"
+    @font_size = 5.334891786
+    erb :input
+  elsif session[:line] == 6
+    @list = "RJAPM"
+    @font_size = 4.267913429
+    erb :input
+  elsif session[:line] == 7
+    @list = "VNQBL"
+    @font_size = 3.73442425
+    erb :input
+  elsif session[:line] == 8
+    @list = "LXCKL"
+    @font_size = 3.200935071
+    erb :input
+  elsif session[:line] == 9
+    @list = "TDIAC"
+    @font_size = 2.66744589282
+    erb :input
+  elsif session[:line] == 10
+    @list = "HEUNG"
+    @font_size = 2.133956714256
+    erb :input
+  elsif session[:line] == 11
+    @list = "JOJIQ"
+    @font_size = 1.600467536
+
+    erb :input
+  elsif session[:line] == 12
+    session[:line] = 1
+    erb :input
+  end
    #strings will automatically go to the browser
 
-  erb :input
+end
+
+
+post '/users/:u_id/tests/vision' do
+  p params
+  if params[:answer] == params[:fiveletters].downcase
+    p "it set"
+    session[:line] += 1
+    redirect '/users/:u_id/tests/vision'
+  else
+    p "did not work"
+    erb :input
+  end
+
 
 end
 
